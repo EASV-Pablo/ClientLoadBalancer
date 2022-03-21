acc7c4d138683f44dfd952c2dc282848cf67f649
EASV-Pablo <pabl0311@easv365.dk>
2022-03-21 21:13:06 +0100

    Refactor

--- Program.cs
+++ Program.cs
@@ -8,28 +8,35 @@ namespace ClientLoadBalancer
 {
     class Program
     {
-        private static string loadBalancerUrl = "https://sunapiserver.herokuapp.com";
+        private static string loadBalancerUrl = "http://localhost:5100";
         private static InputDto input = new InputDto{ Latitude = 55.85463, Longitude = 8.56321, Date = "08/05/2022"};
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            run(10);
+            run(50);
             Console.ReadLine();
         }
 
         public static void run(int loops)
         {
-            while(loops > 0)
+            Random rnd = new Random();
+            while (loops > 0)
             {
-                Task.Run(() => requestSunrise(loops));
-                Thread.Sleep(10);
+                if(rnd.Next(0,2) % 2 == 0 )
+                    Task.Run(() => requestSunrise());
+                else
+                    Task.Run(() => requestSunset());
+
+                Thread.Sleep(rnd.Next(10,50));
                 loops--;
             }
         }
 
-        public static void requestSunset(int num)
+        public static void requestSunset()
         {
+            string initTS = DateTime.Now.ToString();
+
             RestClient c = new RestClient(loadBalancerUrl + "/sun/sunset");
             var request = new RestRequest();
             request.AddJsonBody(input);
@@ -37,9 +44,13 @@ namespace ClientLoadBalancer
             response.Wait();
             OutputDto outp = response.Result;
 
+            string endTS = DateTime.Now.ToString();
+
+            print(outp, initTS, endTS);
+
         }
 
-        public static void requestSunrise(int num)
+        public static void requestSunrise()
         {
             string initTS = DateTime.Now.ToString();
 
@@ -52,12 +63,16 @@ namespace ClientLoadBalancer
 
             string endTS = DateTime.Now.ToString();
 
+            print(outp, initTS, endTS);
+        }
+
+        public static void print(OutputDto outp, string initTS, string endTS)
+        {
             Console.WriteLine("###############################\n" +
-                              "The request num: {0}\n " +
-                              "was executed on machine: {1}\n " +
-                              "Start execution time: {2}\n " +
-                              "End execution time: {3}\n " +
-                              "Result: {4}",num,outp.Machine,initTS,endTS, outp.ToString());
+                              "The request was executed on machine: {0}\n " +
+                              "Start execution time: {1}\n " +
+                              "End execution time: {2}\n "
+                              , outp.Machine, initTS, endTS);
         }
     }    
 }
